453994c1aff723cab801f269d30617c729bf17fc
rongbo <rongbo@qq.com>
2020-06-17 13:48:22 +0800

    nuget push

--- VSIXProject/Command.cs
+++ VSIXProject/Command.cs
@@ -29,9 +29,15 @@ namespace VSIXProject
         private static DTE2 _dte;
 
         /// <summary>
-        /// Command ID.
+        /// DotnetPublishRelease.
         /// </summary>
-        public const int CommandId = 0x0100;
+        public const int DotnetPublishRelease = 0x0100;
+
+        public const int DotnetPublishDebug = 0x0101;
+
+        public const int NugetPushRelease = 0x0102;
+
+        public const int NugetPushDebug = 0x0103;
 
         /// <summary>
         /// Command menu group (command set GUID).
@@ -53,10 +59,10 @@ namespace VSIXProject
         {
             this.package = package ?? throw new ArgumentNullException(nameof(package));
             commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
-
-            var menuCommandID = new CommandID(CommandSet, CommandId);
-            var menuItem = new MenuCommand(this.Execute, menuCommandID);
-            commandService.AddCommand(menuItem);
+            commandService.AddCommand(new MenuCommand(DotnetPublishReleaseExecute, new CommandID(CommandSet, DotnetPublishRelease)));
+            commandService.AddCommand(new MenuCommand(DotnetPublishDebugExecute, new CommandID(CommandSet, DotnetPublishDebug)));
+            commandService.AddCommand(new MenuCommand(NugetPushReleaseExecute, new CommandID(CommandSet, NugetPushRelease)));
+            commandService.AddCommand(new MenuCommand(NugetPushDebugExecute, new CommandID(CommandSet, NugetPushDebug)));
         }
 
         /// <summary>
@@ -97,40 +103,6 @@ namespace VSIXProject
             _dte = dte;
         }
 
-        private void StartNotepad()
-        {
-            try
-            {
-                var projects = (UIHierarchyItem[])_dte?.ToolWindows.SolutionExplorer.SelectedItems;
-                var project = projects[0].Object as Project;
-                var projectDir = Path.GetDirectoryName(project.FullName);
-                var projectPath = Path.Combine(projectDir ?? throw new InvalidOperationException(), "bin\\Release"); ;
-                var command = $" dotnet nuget push {projectPath}\\*.nupkg -k Benchint -s http://10.3.1.240:8080/nuget";
-                System.Diagnostics.Process p = new System.Diagnostics.Process();
-                p.StartInfo.FileName = @"C:\WINDOWS\system32\cmd.exe ";
-                p.StartInfo.UseShellExecute = false;
-                p.StartInfo.RedirectStandardInput = true;
-                p.StartInfo.RedirectStandardOutput = true;
-                p.StartInfo.RedirectStandardError = true;
-                p.StartInfo.CreateNoWindow = true;
-                p.Start();
-                p.StandardInput.WriteLine(projectDir.Substring(0, 2));
-                p.StandardInput.WriteLine($"del {projectPath}\\*.nupkg /s/q");  //先转到系统盘下
-                p.StandardInput.WriteLine($"dotnet pack -s {Path.Combine(projectDir, Path.GetFileName(project.FullName))} -c Release");  //先转到系统盘下
-                p.StandardInput.WriteLine(@"cd C:\WINDOWS\system32 ");  //再转到CMD所在目录下
-                p.StandardInput.WriteLine(command);
-                p.StandardInput.WriteLine("exit");
-                p.WaitForExit();
-                p.Close();
-                p.Dispose();
-                ShowMessageBox("nuget-push success");
-            }
-            catch (Exception ex)
-            {
-                ShowMessageBox(ex.Message);
-            }
-        }
-
         /// <summary>
         /// This function is the callback used to execute the command when the menu item is clicked.
         /// See the constructor to see how the menu item is associated with this function using
@@ -138,7 +110,7 @@ namespace VSIXProject
         /// </summary>
         /// <param name="sender">Event sender.</param>
         /// <param name="e">Event args.</param>
-        private void Execute(object sender, EventArgs e)
+        private void DotnetPublishReleaseExecute(object sender, EventArgs e)
         {
             ThreadHelper.ThrowIfNotOnUIThread();
 
@@ -168,7 +140,120 @@ namespace VSIXProject
             // 创建cmd
             #endregion
 
-            StartNotepad();
+            DotentPublish("Release");
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void DotnetPublishDebugExecute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            DotentPublish("Debug");
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void NugetPushReleaseExecute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            NugetPush("Release");
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void NugetPushDebugExecute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            NugetPush("Debug");
+        }
+
+        private void DotentPublish(string configuration)
+        {
+            System.Diagnostics.Process p = null;
+            try
+            {
+                var projects = (UIHierarchyItem[])_dte?.ToolWindows.SolutionExplorer.SelectedItems;
+                var project = projects[0].Object as Project;
+                var projectDir = Path.GetDirectoryName(project.FullName);
+                var projectPath = Path.Combine(projectDir ?? throw new InvalidOperationException(), $"bin\\{configuration}");
+                p = new System.Diagnostics.Process();
+                p.StartInfo.FileName = @"C:\WINDOWS\system32\cmd.exe ";
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.RedirectStandardInput = true;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.RedirectStandardError = true;
+                p.StartInfo.CreateNoWindow = true;
+                p.Start();
+                p.StandardInput.WriteLine(projectDir.Substring(0, 2));
+                p.StandardInput.WriteLine($"del {projectPath}\\*.nupkg /s/q");  //先转到系统盘下
+                p.StandardInput.WriteLine($"dotnet pack -s {Path.Combine(projectDir, Path.GetFileName(project.FullName))} -c {configuration}");  //先转到系统盘下
+                p.StandardInput.WriteLine(@"cd C:\WINDOWS\system32 ");  //再转到CMD所在目录下
+                p.StandardInput.WriteLine($"dotnet nuget push {projectPath}\\*.nupkg -k Benchint -s http://10.3.1.240:8080/nuget");
+                p.StandardInput.WriteLine("exit");
+                p.WaitForExit();
+                ShowMessageBox("success");
+            }
+            catch (Exception ex)
+            {
+                ShowMessageBox(ex.Message);
+            }
+            finally
+            {
+                p?.Close();
+                p?.Dispose();
+            }
+        }
+
+        private void NugetPush(string configuration)
+        {
+            System.Diagnostics.Process p = null;
+            try
+            {
+                var projects = (UIHierarchyItem[])_dte?.ToolWindows.SolutionExplorer.SelectedItems;
+                var project = projects[0].Object as Project;
+                var projectDir = Path.GetDirectoryName(project.FullName);
+                var projectPath = Path.Combine(projectDir ?? throw new InvalidOperationException());
+                p = new System.Diagnostics.Process();
+                p.StartInfo.FileName = @"C:\WINDOWS\system32\cmd.exe ";
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.RedirectStandardInput = true;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.RedirectStandardError = true;
+                p.StartInfo.CreateNoWindow = true;
+                p.Start();
+                p.StandardInput.WriteLine(projectDir.Substring(0, 2));
+                p.StandardInput.WriteLine($"cd {projectDir.Substring(2)}");
+                p.StandardInput.WriteLine($"del {projectPath}\\*.nupkg /s/q");  //先转到系统盘下
+                p.StandardInput.WriteLine($"nuget pack {project.Name}.csproj -Prop Configuration={configuration}");
+                p.StandardInput.WriteLine($"nuget push *.nupkg -Source http://10.3.1.240:8080/nuget -ApiKey Benchint");
+                p.StandardInput.WriteLine("exit");
+                p.WaitForExit();
+                ShowMessageBox("success");
+            }
+            catch (Exception ex)
+            {
+                ShowMessageBox(ex.Message);
+            }
+            finally
+            {
+                p?.Close();
+                p?.Dispose();
+            }
         }
 
         private void ShowMessageBox(string message)
